dc782cf911141add4dac161d4657b2130734e496
Demo211 <48660421+Demo211@users.noreply.github.com>
2026-05-01 23:12:16 +0300

    Update Plane.cs

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -1,10 +1,7 @@
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Plane : MonoBehaviour
 {
-    public event UnityAction OnCollisionWithCube;
-
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.TryGetComponent<Cube>(out Cube cube))
